c8910d382149b4c542f59cf1fc138be8cf178e75
facupereyrat <faucpereyrat@outlook.com>
2025-09-30 20:16:10 -0300

    Configurando el front-end respecto de la autenticacion

---
+++ AppAsist.Client/Autorizacion/ProveedorAutenticacion.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace AppAsist.Client.Autorizacion
+{
+    public class ProveedorAutenticacion : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(5000);
+            var anonimo = new ClaimsIdentity();
+            var usuarioPepe = new ClaimsIdentity(
+                new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "Pepe Sanchez"),
+                    new Claim(ClaimTypes.Role, "operador"),
+                    new Claim("DNI", "12.587.895"),
+                },
+                authenticationType: "ok"
+                );
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimo)));
+        }
+    }
+}
+
--- AppAsist.Client/Program.cs
+++ AppAsist.Client/Program.cs
@@ -1,5 +1,7 @@
 using AppAsist.Client;
+using AppAsist.Client.Autorizacion;
 using AppAsist.Client.Servicios;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Proyecto2024.Client.Servicios;
@@ -11,10 +13,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // Registrar el HttpClient
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddAuthorizationCore();
 
 // Registrar IHttpServicio con su implementación HttpServicio
 builder.Services.AddScoped<IHttpServicio, HttpServicio>();
-
+builder.Services.AddScoped<AuthenticationStateProvider, ProveedorAutenticacion>();
 // Construir y ejecutar la aplicación
 await builder.Build().RunAsync();
